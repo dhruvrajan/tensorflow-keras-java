c94cc4fb0add807cce9466a2445ec99e1a40159e
Dhruv Rajan <dhruv@krishnaprem.com>
2019-09-26 16:15:34 -0700

    tensor frame classifier works fast

--- src/main/java/org/tensorflow/keras/optimizers/GradientDescentOptimizer.java
+++ src/main/java/org/tensorflow/keras/optimizers/GradientDescentOptimizer.java
@@ -11,17 +11,22 @@ import java.util.List;
 
 public class GradientDescentOptimizer extends Optimizer<Float> {
   private final float LEARNING_RATE;
+  Constant<Float> alpha;
 
   public GradientDescentOptimizer(float lr) {
     this.targets = new ArrayList<>();
     this.LEARNING_RATE = lr;
   }
 
+  @Override
+  public void build(Ops tf) {
+    this.alpha = tf.constant(LEARNING_RATE);
+  }
+
   public List<Operand<Float>> applyGradients(
       Ops tf, List<Variable<Float>> weights, Gradients gradients) {
     tf = tf.withName("GradientDescentOptimizer");
 
-    Constant<Float> alpha = tf.constant(LEARNING_RATE);
     for (int i = 0; i < weights.size(); i++) {
       targets.add(tf.applyGradientDescent(weights.get(i), alpha, gradients.dy(i)));
     }
