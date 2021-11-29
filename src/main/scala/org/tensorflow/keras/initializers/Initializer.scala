0fecbdc2801787fee684b6cf3d66664bf31ad29e
Hanns Holger Rutz <contact@sciss.de>
2021-11-29 12:30:21 +0100

    wip <>

--- src/main/scala/org/tensorflow/keras/initializers/Initializer.scala
+++ src/main/scala/org/tensorflow/keras/initializers/Initializer.scala
@@ -7,6 +7,38 @@ import org.tensorflow.op.core.Assign
 import org.tensorflow.types.TInt32
 import org.tensorflow.types.family.TNumber
 
+object Initializer {
+  /** Computes the number of input and output units for a weight shape.
+    *
+    * @param shape  Integer shape tuple or TF tensor shape.
+    * @return       A tuple of integer scalars (fan_in, fan_out).
+    */
+  def computeFans(shape: Operand[TInt32]): (Int, Int) = {
+    shape.rank() match {
+      case i if i < 1 => (1, 1)
+      case 1 =>
+//        val fan = shape(0)
+//        (fan, fan)
+        ???
+      case 2 =>
+//        (shape(0), shape(1))
+        ???
+      case _ =>
+        // Assuming convolution kernels (2D, 3D, or more).
+        // kernel shape: (..., input_depth, depth)
+        var receptiveFieldSize = 1
+//        for (dim <- shape(/*:- 2*/)) {
+//          receptiveFieldSize *= dim
+//        }
+//        (
+//          shape(-2) * receptiveFieldSize,
+//          shape(-1) * receptiveFieldSize
+//        )
+        ???
+    }
+  // return int(fan_in), int(fan_out)
+  }
+}
 abstract class Initializer {
   /**
     * Adds an `Assign` Op to the graph to initialize
@@ -16,7 +48,8 @@ abstract class Initializer {
     * @param in Variable to initialize
     * @return Assign Operand created
     */
-  def apply[T <: TNumber](tf: Ops, in: Operand[T], dtype: Class[T]): Assign[T] = tf.assign(in, this.initialize(tf, Keras.shapeOperand(tf, in.asOutput.shape), dtype))
+  def apply[T <: TNumber](tf: Ops, in: Operand[T], dtype: Class[T]): Assign[T] =
+    tf.assign(in, this.initialize(tf, Keras.shapeOperand(tf, in.asOutput.shape), dtype))
 
   /**
     * Returns a Tensor object initialized as
